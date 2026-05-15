aa6de5d27354614da84ae0f96c6f2d03a9164a1e
sj0724 <sj07245@naver.com>
2026-05-15 19:02:45 +0900

    init: 테일윈드 컬러 및 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="font-pretendard">{children}</body>
     </html>
   )
 }
