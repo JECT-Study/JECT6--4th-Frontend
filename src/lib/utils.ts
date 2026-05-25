00b0d669f6ba64053489332247202843c85db089
sj0724 <sj07245@naver.com>
2026-05-25 21:55:31 +0900

    fix: shadcn 버튼으로 수정

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,14 @@
 import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { extendTailwindMerge } from 'tailwind-merge'
+
+// tailwind-merge를 확장하여 사용자 정의 클래스 그룹을 추가
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      'font-size': [{ text: ['12', '14', '16', '18', '20', '22', '24', '28', '36', '40', '56'] }],
+    },
+  },
+})
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
