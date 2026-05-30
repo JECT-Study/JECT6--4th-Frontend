dcdd8fec223b995027d144a3812e8b6056470586
sj0724 <sj07245@naver.com>
2026-05-30 12:04:11 +0900

    feat: header, footer 구현

---
+++ src/app/_components/Footer.tsx
@@ -0,0 +1,12 @@
+export function Footer() {
+  return (
+    <footer className="w-screen bg-[#828282]">
+      <div className="max-w-360 px-30 flex flex-col items-center justify-between mx-auto">
+        <div className="flex gap-4 items-center h-27.5 w-full my-6" />
+        <div className="text-16 leading-24 text-neutral_80 py-4">
+          © 2026 JECT. All rights reserved.
+        </div>
+      </div>
+    </footer>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
+import { Footer } from './_components/Footer'
+import { Header } from './_components/Header'
 import './globals.css'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="font-pretendard">{children}</body>
+      <body className="font-pretendard">
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
